8a5494e2791633113096f28ecd8764defca92c05
Somon Das <somondas067@gmail.com>
2025-08-15 23:47:43 +0530

    Updates

--- supabase-auth/src/router.jsx
+++ supabase-auth/src/router.jsx
@@ -3,10 +3,18 @@ import Dashboard from "./components/Dashboard";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import App from "./App";
+import PrivateRoute from "./components/PrivateRoute";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/dashboard", element: <Dashboard /> },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
+  },
   { path: "/sign-in", element: <Signin /> },
   { path: "/sign-up", element: <Signup /> },
 ]);
